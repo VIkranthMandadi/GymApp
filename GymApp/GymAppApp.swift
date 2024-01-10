208064408db480855064db2d7e0b411bb4070aa6
vikranth <vikmandadi@gmail.com>
2024-01-10 14:57:08 +0530

    Initial Commit

---
+++ GymApp/GymAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymAppApp.swift
+//  GymApp
+//
+//  Created by Vikranth Mandadi on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GymAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
